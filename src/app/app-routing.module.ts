6644a2520efdb24eac4aca56424fd49623b2b5c8
kran97 <kgupta.1697@gmail.com>
2019-10-14 16:50:43 +0530

    monday

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from "./components/login/login.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { AuthGuard } from "../auth.guard";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: '', component: DashboardComponent, canActivate:[AuthGuard]
+  },
+  {
+    path: '**', redirectTo: ''
+  }
+];
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { AdminLogin } from "../../models/login.model";
+import * as $ from 'jquery';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  adminData: AdminLogin = new AdminLogin();
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    $("#submitLogin").click(function (e) {
+      e.preventDefault();
+    });
+  }
+
+  login() {
+    this.adminData = {
+      email: $("#email").val(),
+      password: $("#password").val()
+    }
+    
+    $.ajax({
+      type: 'POST',
+      url: "http://fundoonotes.incubation.bridgelabz.com/api/user/adminLogin",
+      data: this.adminData,
+      dataType: "json",
+      success: (response) => {
+        localStorage.setItem('token', response.id);
+        this.router.navigate(['/dashboard'])
+        console.log(" login successful ", response);
+      },
+      error : (error) => {
+        console.log(error);
+      }
+    });
+
+  }
+}
